39636e38c35f09b0b2d170ea6f784a56a9f1ec70
sravaniv <sravani.vyakaranam@gmail.com>
2022-10-12 13:23:06 +0530

    Enable the apache service

--- automation.sh
+++ automation.sh
@@ -7,15 +7,14 @@ s3_bucket="upgrad-sravani"
 sudo apt update -y
 sudo apt install apache2
 service apache2 status
+systemctl enable apache2.service
 
 cd /var/log/apache2/
-tar -cf ${myname}-httpd-logs-${timestamp}.tar *.log
+tar -cf /tmp/${myname}-httpd-logs-${timestamp}.tar *.log
 
 sudo apt update -y
 sudo apt install awscli -y
 
 aws s3 \
-cp ${myname}-httpd-logs-${timestamp}.tar \
+cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
-
-
